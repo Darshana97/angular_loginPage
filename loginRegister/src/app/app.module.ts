6df11a19ff046f56df95a743fa882ffc7d2b365f
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-03-10 09:31:28 +0530

    angular login components

--- loginRegister/src/app/app.module.ts
+++ loginRegister/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { AddComponent } from './students/add/add.component';
+import { ViewComponent } from './students/view/view.component';
+import { EditComponent } from './students/edit/edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    AddComponent,
+    ViewComponent,
+    EditComponent
   ],
   imports: [
     BrowserModule,
